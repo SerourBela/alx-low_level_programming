9c8cd15104516ba2526f794b8acb14e1f2201e21
Serour Belabed <68053753+SerourBela@users.noreply.github.com>
2023-09-08 13:31:46 +0100

    Update

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Prints the numbers from 00 to 99, numbers separated by
+ *        a comma followed by a space, in ascending order.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int digit1, digit2;
